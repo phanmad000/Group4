8c2299786a60634085249e217e4aa9812698be25
phanmad000 <42547660+phanmad000@users.noreply.github.com>
2018-11-14 10:41:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+//dataset
+const sample = [
+  {
+    animal: 'Chicken',
+    depositedGas: '22.2'
+  },
+  
+
+// margin and radius
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+      width= 100 - margin.right - (.5*margin.left), 
+      height = 100 - margin.top - margin.bottom
+      radius = width/2
+
+// arc generator
+var arc = d3.arc()
+  .outerRadius(radius - 10)
+  .innerRadius(0);
+
+
 
